537057161681326e1cbe4e94973bb7205fb51451
einsss <einsss@gmail.com>
2022-10-31 18:30:20 +0900

    https://github.com/einsss/springbootdemo.git

---
+++ src/main/java/com/spring/boot/SpringBootDemo/TestControllerSimple.java
@@ -0,0 +1,13 @@
+package com.spring.boot.SpringBootDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestControllerSimple {
+
+	@RequestMapping("/hello")
+	public String test() {
+		return "Spring test"; 
+	}
+}
